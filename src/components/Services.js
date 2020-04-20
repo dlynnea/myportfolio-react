ba2fd1e0b03cf2da6bbc57c4e2a30fc599c624d7
dlynnea <danimatton@gmail.com>
2020-04-20 15:24:39 -0600

    services

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,8 +2,60 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            <h1>Services</h1>
+        <div className="services">
+            <div className="container">
+                <div className="row">
+                    <div className="section-title text-center">
+                        <h1>My Services</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="service-content">
+                        <div className="box">
+                            <div className="icon">
+                                <i className="fa fa-laptop"></i>
+                            </div>
+                                <h5>Unique Design</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="box">
+                            <div className="icon">
+                                <i className="fa fa-columns"></i>
+                            </div>
+                                <h5>Unique Design</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="box">
+                            <div className="icon">
+                                <i className="fa fa-code"></i>
+                            </div>
+                                <h5>Unique Design</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="box">
+                            <div className="icon">
+                                <i className="fa fa-mobile"></i>
+                            </div>
+                                <h5>Unique Design</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="box">
+                            <div className="icon">
+                                <i className="fa fa-search"></i>
+                            </div>
+                                <h5>Unique Design</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="box">
+                            <div className="icon">
+                                <i className="fa fa-cog"></i>
+                            </div>
+                                <h5>Unique Design</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
