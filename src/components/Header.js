2fff401e648e827fa912d07ce80a66e739298c50
dlynnea <danimatton@gmail.com>
2020-04-20 10:11:45 -0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { BrowserRouter as Link } from 'react-router-dom';
+
+export function Header() {
+    return (
+        <header>
+            <div className="container">
+                <div className="row">
+                <div className="brand-name">
+                    <Link className="logo" to="/">Danielle</Link>
+                </div>
+                <div className="navbar">
+                    <ul>
+                        <li>
+                            <Link className="link" to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/">About</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/">Services</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/">Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/">Blog</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/">Contact</Link>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+
+
+
+
---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Link } from 'react-router-dom';
+
+
+export const Home = () => {
+    return (
+        <div className="home">
+            <div className="container">
+                <div className="row">
+                    <div className="home-content">
+                        <div className="block">
+                            <h6>Hello, My Name is</h6>
+                            <h1>Dani Matton</h1>
+                            <h3>Web Developer</h3>
+                            <div className="cv-btn">
+                                <Link to="/">Download CV</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
