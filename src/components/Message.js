7fab55b7ed65de98db9658ba6ad91af079bb0d85
dlynnea <danimatton@gmail.com>
2020-04-23 21:26:15 -0600

    message component finished

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,7 +4,14 @@ export default function Message() {
     return (
         <div className="message-text">
             <div className="container">
-                <div className="row"></div>
+                <div className="row align-items-center">
+                    <div className="text">
+                        <h2>Contact me if you'd like!</h2>
+                    </div>
+                    <div classname="button text-center">
+                        <a href="">Get in touch</a>
+                    </div>
+                </div>
             </div>  
         </div>
     )
