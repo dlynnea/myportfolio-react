8d9ffef08a1717bde082a58222aa20ed31bad170
dlynnea <danimatton@gmail.com>
2020-04-24 18:27:29 -0600

    blogs component cleanup, still with filler images and lorem ipsum text

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -10,46 +10,16 @@ class Blogs extends Component {
             blogs: [
                 {
                     id: 0,
-                    title: 'HTTP & MVC Architecture',
-                    subtitle: 'Ruby on Rails',
+                    title: 'Getting Started with Node.js',
+                    subtitle: 'Node.js',
                     summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                    img: three,
-                    date: '7 Jan 2020',
-                    link: 'https://medium.com/@danimatton/taking-a-look-at-http-mvc-architecture-and-routes-through-ruby-on-rails-cac9ec742e0a',
+                    img: one,
+                    date: '21 April 2020',
+                    link: 'https://medium.com/@danimatton/getting-started-with-node-js-e4717cbc2ace',
                     selected: false
                 },
                 {
                     id: 1,
-                    title: 'Forms & FormData',
-                    subtitle: 'JavaScript',
-                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                    img: one,
-                    date: '29 Jan 2020',
-                    link: 'https://medium.com/@danimatton/forms-formdata-and-javascript-32d97bcd28d8',
-                    selected: false
-                },
-                {
-                    id: 2,
-                    title: 'SQL vs. NoSQL',
-                    subtitle: 'Databases',
-                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                    img: three,
-                    date: '19 Feb 2020',
-                    link: 'https://medium.com/@danimatton/sql-vs-nosql-whats-the-deal-93b4943a2ce6',
-                    selected: false
-                },
-                {
-                    id: 3,
-                    title: 'Redux Middleware',
-                    subtitle: 'Middleware',
-                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                    img: one,
-                    date: '11 March 2020',
-                    link: 'https://medium.com/@danimatton/redux-middleware-what-is-it-why-use-it-ea18e0433fa0',
-                    selected: false
-                },
-                {
-                    id: 4,
                     title: 'WebSockets',
                     subtitle: 'WebSockets, Socket.io',
                     summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
@@ -59,19 +29,50 @@ class Blogs extends Component {
                     selected: false
                 },
                 {
-                    id: 5,
-                    title: 'Getting Started with Node.js',
-                    subtitle: 'Node.js',
+                    id: 2,
+                    title: 'Redux Middleware',
+                    subtitle: 'Middleware',
                     summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
                     img: one,
-                    date: '21 April 2020',
-                    link: 'https://medium.com/@danimatton/getting-started-with-node-js-e4717cbc2ace',
+                    date: '11 March 2020',
+                    link: 'https://medium.com/@danimatton/redux-middleware-what-is-it-why-use-it-ea18e0433fa0',
                     selected: false
                 },
+                {
+                    id: 3,
+                    title: 'SQL vs. NoSQL',
+                    subtitle: 'Databases',
+                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    img: three,
+                    date: '19 Feb 2020',
+                    link: 'https://medium.com/@danimatton/sql-vs-nosql-whats-the-deal-93b4943a2ce6',
+                    selected: false
+                },
+                {
+                    id: 4,
+                    title: 'Forms & FormData',
+                    subtitle: 'JavaScript',
+                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    img: one,
+                    date: '29 Jan 2020',
+                    link: 'https://medium.com/@danimatton/forms-formdata-and-javascript-32d97bcd28d8',
+                    selected: false
+                },
+                {
+                    id: 5,
+                    title: 'HTTP & MVC Architecture',
+                    subtitle: 'Ruby on Rails',
+                    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    img: three,
+                    date: '7 Jan 2020',
+                    link: 'https://medium.com/@danimatton/taking-a-look-at-http-mvc-architecture-and-routes-through-ruby-on-rails-cac9ec742e0a',
+                    selected: false
+                }
             ]
-        }}
-    render() {
+        }
+    }
 
+    render() {
         return (
             <div className="blogs">
                 <div className="container">
@@ -91,7 +92,7 @@ class Blogs extends Component {
                                     <span>{blog.date}</span> <span>{blog.subtitle}</span>
                                 </div>
                                 <h3>{blog.title}</h3>
-                                <p>{blog.subtitle}</p>
+                                <p>{blog.summary}</p>
                                 <a target='_blank' href={blog.link}>Read the Article Here</a>
                             </div>
                             ))}
