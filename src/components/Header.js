268213697a3b2ab487c2406fa879cc30452b73bc
dlynnea <danimatton@gmail.com>
2020-05-04 23:29:17 -0600

    deploying without skills component

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,41 +38,6 @@ export function Header(props) {
                 </div>
             </nav>
         </header>
-
-        // <header>
-        //     <div className="container">
-        //         <div className="row">
-        //         <div className="brand-name">
-        //             <Link className="logo" to="/">D /M</Link>
-        //         </div>
-        //         <div className="hamburger" onClick={handleClick}>
-        //             <i className="fa fa-bars"></i>
-        //         </div>
-        //         <div className="navbar">
-        //             <ul>
-        //                 <li>
-        //                     <Link to="/"><a href="#home">Home</a></Link>
-        //                 </li>
-        //                 <li>
-        //                     <a href="#about">About</a>
-        //                 </li>
-        //                 <li>
-        //                     <Link to="/portfolio">Portfolio</Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to="/blogs">Blogs</Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to="/resume">Resume</Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to="/contact">Contact</Link>
-        //                 </li>
-        //             </ul>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </header>
     )
 }
 
