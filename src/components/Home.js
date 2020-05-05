67b0f39f959eae3ccddbb3d00aca0f73c81a6af8
dlynnea <danimatton@gmail.com>
2020-05-04 23:31:20 -0600

    resume link in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,9 +10,9 @@ export const Home = () => {
                         <div className="block">
                             <h1>Dani Matton</h1>
                             <h3>Full Stack Web Developer, Software Engineer, Designer</h3>
-                            {/* <div className="cv-btn">
+                            <div className="cv-btn">
                                 <a href={PDF} target="_blank" rel="noopener noreferrer">Resume</a>
-                            </div> */}
+                            </div>
                         </div>
                     </div>
                 </div>
