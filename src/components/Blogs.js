dea8d7e3396bb4fb33db58e5432a3181a316395d
dlynnea <danimatton@gmail.com>
2020-05-07 17:35:15 -0600

    included newest blog post and some blog styling

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -5,6 +5,8 @@ import database from '../images/database.jpg';
 import architecture from '../images/architecture.jpg';
 import sockets from '../images/sockets.jpg';
 import form from '../images/form.jpeg';
+import jwt from '../images/jwt.jpg';
+import nodejwt from '../images/node-jwt.jpeg';
 
 class Blogs extends Component {
 
@@ -12,6 +14,16 @@ class Blogs extends Component {
         blogs: [
             {
                 id: 0,
+                title: 'JWT Implementation for Node.js',
+                subtitle: 'Node.js, JWT',
+                summary: 'An abbreviated example of including JWT authentication within your Node API. It should give you an idea of how to protect your routes, get a token and then how to use that token to access the protected routes.',
+                img: nodejwt,
+                date: '7 May 2020',
+                link: 'https://medium.com/@danimatton/jwt-implementation-for-node-js-cca3d64b6b50',
+                selected: false
+            },
+            {
+                id: 1,
                 title: 'Getting Started with Node.js',
                 subtitle: 'Node.js',
                 summary: 'Node is primarily used for non-blocking, event-driven servers because of its single-threaded nature. It is used for traditional web applications and backend API services but was also designed for real-time websites with push-capability.',
@@ -21,7 +33,7 @@ class Blogs extends Component {
                 selected: false
             },
             {
-                id: 1,
+                id: 2,
                 title: 'WebSockets',
                 subtitle: 'WebSockets, Socket.io',
                 summary: 'WebSockets allow for a bi-directional communication channel, meaning data flows both ways. The client and the server can talk in real-time without having to continuously make requests.',
@@ -31,7 +43,7 @@ class Blogs extends Component {
                 selected: false
             },
             {
-                id: 2,
+                id: 3,
                 title: 'Redux Middleware',
                 subtitle: 'Middleware',
                 summary: 'Middleware provides a way to interact with actions that have been dispatched to the store before they reach the store’s reducer. Some uses for middleware would be logging actions, reporting errors, making asynchronous requests, as well as dispatching new actions.',
@@ -41,7 +53,7 @@ class Blogs extends Component {
                 selected: false
             },
             {
-                id: 3,
+                id: 4,
                 title: 'SQL vs. NoSQL',
                 subtitle: 'Databases',
                 summary: 'Choosing a noSQL database over RDBMS are more prominent when needing to work with big data, when the big data needs a flexible data model with a better database architecture, or in the case of processing big data, the databases need continuous application availability with modern transaction support.',
@@ -51,7 +63,7 @@ class Blogs extends Component {
                 selected: false
             },
             {
-                id: 4,
+                id: 5,
                 title: 'Forms & FormData',
                 subtitle: 'JavaScript',
                 summary: 'Traditionally, a form was the only way to send data, in which the client had entered, back to the server. <form> is an HTML tag that allows you to perform GET / POST / UPDATE / DELETE, etc. operations without writing any code in JavaScript or server-side, in fact forms were designed for a pre-JavaScript Web.',
@@ -61,7 +73,7 @@ class Blogs extends Component {
                 selected: false
             },
             {
-                id: 5,
+                id: 6,
                 title: 'HTTP & MVC Architecture',
                 subtitle: 'Ruby on Rails',
                 summary: 'MVC separates an application into three components. The Models for handling data and business logic, the Controllers for handling the user interface and application, and Views for handling graphical user interface objects and presentation.',
